e78d3417e7c829a4a71c0cf9c081783d907a6d8c
aicedrev <josephverdecia@gmail.com>
2022-02-22 21:06:56 -0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,7 +48,19 @@ skillsHeader.forEach((el) =>{
 	el.addEventListener('click', toggleSkills)
 })
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+			tabContents = document.querySelectorAll('[data-content]')
 
+tabs.forEach(tab =>{
+	tab.addEventListener('click', () =>{
+		const target = document.querySelector(tab.dataset.target)
+
+		tabContents.forEach(tabContent =>{
+			tabContent.classList.remove('qualification__active')
+		})
+		target.classList.add
+	})
+})
 
 /*==================== SERVICES MODAL ====================*/
 
