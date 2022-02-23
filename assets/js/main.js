ebeb302326e6df300df913801f9f8302676f1c93
aicedrev <josephverdecia@gmail.com>
2022-02-23 13:41:16 -0800

    Qualification button

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,7 +58,12 @@ tabs.forEach(tab =>{
 		tabContents.forEach(tabContent =>{
 			tabContent.classList.remove('qualification__active')
 		})
-		target.classList.add
+		target.classList.add('qualification__active')
+
+		tab.forEach(tab =>{
+			tab.classList.remove('qualification__active')
+		})
+		tab.classList.add('qualification__active')
 	})
 })
 
